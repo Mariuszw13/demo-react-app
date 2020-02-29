1cef551fbf791f6a3d380bad563e8e928d09362f
Mariusz <>
2020-02-29 19:07:27 +0100

    Updates

---
+++ precache-manifest.94c4cdc02900627337cb805cb8751b80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f246cec488ce94147bcbb8c6da7898b6",
+    "url": "/demo-react-app/index.html"
+  },
+  {
+    "revision": "b22ef16d5cd875355740",
+    "url": "/demo-react-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b22ef16d5cd875355740",
+    "url": "/demo-react-app/static/js/2.7db2ae3a.chunk.js"
+  },
+  {
+    "revision": "ba0e32885df4ac07678f28be2b5acf7c",
+    "url": "/demo-react-app/static/js/2.7db2ae3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b071d0d9224470a5e2b3",
+    "url": "/demo-react-app/static/js/main.986d0b38.chunk.js"
+  },
+  {
+    "revision": "f49a969c66de591c4e2f",
+    "url": "/demo-react-app/static/js/runtime-main.4a2fc875.js"
+  }
+]);
