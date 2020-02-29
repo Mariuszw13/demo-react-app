64665ac77c68e3deaec73a081b57fc6158ab0ee4
Mariusz <>
2020-02-29 18:57:51 +0100

    Move api url to env variable

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
-import {noop} from "../utils";
+import axios from "axios";
+import { noop } from "../utils";
 
-const instance = axios.create({baseURL: "https://my-json-server.typicode.com/Mariuszw13/db-mock"});
+const instance = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 export const tryLogin = async (credentials, onSuccess, onFailure = noop) => {
     try {
@@ -16,7 +16,7 @@ export const tryLogin = async (credentials, onSuccess, onFailure = noop) => {
 
 export const getItems = async (onSuccess, token, onFailure = noop) => {
     try {
-        const config = {headers: {"X-Token": token}};
+        const config = { headers: { "X-Token": token } };
         const response = await instance.get("/items", config);
         onSuccess(response.data);
     } catch (e) {
@@ -27,22 +27,22 @@ export const getItems = async (onSuccess, token, onFailure = noop) => {
 
 export const deleteItem = async (onSuccess, token, id, onFailure = noop) => {
     try {
-        const config = {headers: {"X-Token": token}};
+        const config = { headers: { "X-Token": token } };
         const response = await instance.delete(`/items/${id}`, config);
         onSuccess(response.data);
     } catch (e) {
         console.log(e);
-        onFailure()
+        onFailure();
     }
 };
 
 export const addItem = async (onSuccess, token, newItem, onFailure = noop) => {
     try {
-        const config = {headers: {"X-Token": token}};
+        const config = { headers: { "X-Token": token } };
         const response = await instance.post("/items", newItem, config);
         onSuccess(response.data);
     } catch (e) {
         console.log(e);
-        onFailure()
+        onFailure();
     }
 };
